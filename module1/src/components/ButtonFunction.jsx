496f75d2386b4cff26cc4a27a34bb72dc8e933ca
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-08-23 21:52:49 +0200

    refactor

--- module1/src/components/ButtonFunction.jsx
+++ module1/src/components/ButtonFunction.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
-function logRandom() {
-    console.log(Math.random());
-}
 
 function ButtonFunction() {
-    //const[currentSatateValue,functionToSetNewStateValue] =useState(initialStateValue);
+    //const[currentSatateValue,functionToSetNewStateValue] =useState(initialStateValue);    
     const [counter, setCounter] = useState(0);
+
+    const increment = (e) => {
+        e.preventDefault();
+        setCounter(counter + 1);
+    };
+
     return <>
-        <button onClick={logRandom}>Hello ! push me! {Math.random()}</button>
-        <button onClick={() => console.log(Math.random())}>Hello ! push me! {Math.random()}</button>
-        <button onClick={() => setCounter(counter + 1)}>Count - {counter}</button>
+        <button type="button"
+            onClick={(e) => increment(e)}>Count - {counter}
+        </button>
     </>
 
 }
