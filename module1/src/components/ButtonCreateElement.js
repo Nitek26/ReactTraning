3f04a112a5469e8489b8ede9b63375aaba04b42c
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-08-23 22:06:57 +0200

    add component using CreateElement

---
+++ module1/src/components/ButtonCreateElement.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class ButtonCreateElement extends React.Component {
+    render() {
+        return React.createElement('button', null, `A to jest component  bez jsx`);
+    }
+}
+
+export default ButtonCreateElement;
