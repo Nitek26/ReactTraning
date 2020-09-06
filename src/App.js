30b15a07eb0e7908e23524f9bef34a6484d62d31
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-09-06 07:27:28 +0200

    add some empty components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
-import ErrorBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 function App() {
   return (
