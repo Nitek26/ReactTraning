a7990c541728263417b54fdb2510d636e80882ef
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-09-18 12:35:00 +0200

    style filter/order bar

--- src/components/Collection/FilterBar/FilterBar.jsx
+++ src/components/Collection/FilterBar/FilterBar.jsx
@@ -3,7 +3,16 @@ import './FilterBar.css'
 
 class FilterBar extends React.Component {
     render() {
-        return <div className="filter-bar">FilterBar</div>
+        return <div className="filter-bar">
+            <div class="filters">
+                <button>ALL</button>
+                <button>DOCUMENTARY</button>
+                <button>COMEDY</button>
+                <button>HORROR</button>
+                <button>CRIME</button>
+            </div>
+            <div className="movie-counter">7 movies found</div>
+        </div>
     }
 }
 
--- src/components/Collection/OrderBar/OrderBar.jsx
+++ src/components/Collection/OrderBar/OrderBar.jsx
@@ -3,7 +3,20 @@ import './OrderBar.css'
 
 class OrderBar extends React.Component {
     render() {
-        return <div className="order-bar">OrderBar</div>
+        return <div className="order-bar">
+
+            <div class="bar">
+                <span>SORT BY</span>
+                <div className="select-bar-dropdown">
+                    <select>
+                        <option value="relase_date">RELEASE DATE</option>
+                        <option value="views">VIEWS</option>
+                        <option value="title">TITLE</option>
+                        <option value="stars">STARS</option>
+                    </select>
+                </div>
+            </div>
+        </div>
     }
 }
 
