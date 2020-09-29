c70ed9dcbcaec59dffa0f2cfa99fa5502f739a1c
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-09-29 21:31:32 +0200

    add simple form for add movier modal

--- src/components/Header/AddMovie/AddMovie.jsx
+++ src/components/Header/AddMovie/AddMovie.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import './AddMovie.css'
+import './../../../data/MovieGenres.js';
+import genres from './../../../data/MovieGenres.js';
 
 function AddMovie() {
     const [show, setShow] = useState(false);
@@ -16,12 +18,46 @@ function AddMovie() {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>ADD MOVIE</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formTitle">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control placeholder="Title" />
+                        </Form.Group>
+                        <Form.Group controlId="formReleaseDate">
+                            <Form.Label>Release Date</Form.Label>
+                            <Form.Control placeholder="Select Date" />
+                        </Form.Group>
+                        <Form.Group controlId="formUrl">
+                            <Form.Label>Movie URL</Form.Label>
+                            <Form.Control placeholder="Movie URL here" />
+                        </Form.Group>
+                        <Form.Group controlId="formGenre">
+                            <Form.Label>Genre</Form.Label>
+                            <Form.Control as="select">
+                                <option default>Select Genre</option>
+                                <option>{genres.DOCUMENTARY}</option>
+                                <option>{genres.COMEDY}</option>
+                                <option>{genres.HORROR}</option>
+                                <option>{genres.CRIME}</option>
+                                <option>{genres.DRAMA}</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="formOverview">
+                            <Form.Label>Overview</Form.Label>
+                            <Form.Control placeholder="Overview here" />
+                        </Form.Group>
+                        <Form.Group controlId="formRuntime">
+                            <Form.Label>Runtime</Form.Label>
+                            <Form.Control placeholder="Runtime here" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>Close</Button>
-                    <Button variant="primary" onClick={handleClose}>Save Changes</Button>
+                    <Button variant="secondary" onClick={handleClose}>RESET</Button>
+                    <Button variant="primary" onClick={handleClose}>SUBMIT</Button>
                 </Modal.Footer>
             </Modal>
         </>
