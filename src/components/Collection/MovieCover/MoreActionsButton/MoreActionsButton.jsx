f8ddf8902f755f1a8aed6ed80cc9a15fccc71bf4
Aleksandra Nitka <Aleksandra_Nitka@epam.com>
2020-10-10 19:07:20 +0200

    add dropdown menu for more action button

--- src/components/Collection/MovieCover/MoreActionsButton/MoreActionsButton.jsx
+++ src/components/Collection/MovieCover/MoreActionsButton/MoreActionsButton.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './MoreActionsButton.css'
+import { Dropdown } from 'react-bootstrap';
 
-class MoreActionsButton extends React.Component {
-    render() {
-        return <button className="more-btn"></button>
-    }
+function MoreActionsButton() {
+
+    return (
+        <>
+            <Dropdown>
+                <Dropdown.Toggle className="more-btn" id="dropdown-basic" tag="button" type="button">
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </>
+    )
 }
 
 export default MoreActionsButton;
